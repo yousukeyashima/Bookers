d13b6da3f27704189669cee3519b3c240b85f5b5
vagrant <vagrant@localhost.localdomain>
2020-05-16 07:43:03 +0000

    [Add]課題：アプリケーションを作成してみよう：基礎編

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	@book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+    redirect_to book_path(@book.id), notice: "Book was successfully created."
+  else
+    render action: :index
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+  	if @book.update(book_params)
+  	 redirect_to book_path(@book), notice: "Book was successfully update."
+    else
+     render action: :edit
+    end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
